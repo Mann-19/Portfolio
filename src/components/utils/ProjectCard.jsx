71ea3e7c3ca9704d1de0652364f1780bfa7babb2
Mann-19 <tkp2510@gmail.com>
2025-01-18 14:35:03 +0530

    "Project cards UI changes"

--- src/components/utils/ProjectCard.jsx
+++ src/components/utils/ProjectCard.jsx
@@ -8,7 +8,32 @@ const ProjectCard = ({ name, className, tags, desc, githubLink }) => {
     <div
       className={`${className} grid-item flex flex-col gap-5 md:gap-0 md:justify-between`}
     >
-      <h2 className="">{name}</h2>
+      <h2 className="flex md:items-center md:gap-8 md:justify-normal justify-between">
+        {name}
+        <div className="md:block hidden">
+          {githubLink && (
+            <button
+              type="button"
+              className="text-base z-20 font-normal flex items-center gap-2 bg-black bg-opacity-70 px-3 py-1 rounded-xl hover:bg-opacity-60 active:border"
+              onClick={handleClick}
+            >
+              GitHub
+              <img src="/github.svg" alt="" className="w-5 aspect-square" />
+            </button>
+          )}
+        </div>
+        <div className="md:hidden block">
+          {githubLink && (
+            <button
+              type="button"
+              className="rounded-full bg-black border-black border-[5px]"
+              onClick={handleClick}
+            >
+              <img src="/github.svg" alt="" className="w-6 aspect-square" />
+            </button>
+          )}
+        </div>
+      </h2>
 
       <p className="text-base font-Roboto font-light text-primaryLight">
         {desc && desc}
@@ -19,21 +44,12 @@ const ProjectCard = ({ name, className, tags, desc, githubLink }) => {
           tags.map((tag, index) => (
             <span
               key={index}
-              className="text-sm bg-primaryLight rounded-lg text-primaryDark px-2 py-1"
+              className="text-sm bg-primaryLight rounded-lg text-primaryDark px-2 py-1 flex items-center gap-1"
             >
-              {tag}
+              <i className={`${tag.iconClass}`}></i>
+              {tag?.name}
             </span>
           ))}
-      </div>
-
-      <div>
-        {githubLink && (
-          <button type="button" className="text-base z-20 font-normal flex items-center gap-2 bg-black px-3 py-1 rounded-xl hover:bg-opacity-60 active:border" onClick={handleClick}>
-            GitHub
-            <img src="/github.svg" alt="" className="w-5 aspect-square" />
-          </button>
-        )}
-        {/* {props.links && <button><img src="" alt="" /></button>} */}
       </div>
     </div>
   );
